4c730587dcbb2416324d970c1aa10779b9c704c3
Dheemanth_Sunkara <121927018+Dheemanth-Chowdary@users.noreply.github.com>
2023-04-01 10:10:57 +0530

    Add files via upload

---
+++ SIMATS Programs/1) Reverse of String using loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char word[50],reverse[50];
+	int i,j;
+	printf("Enter a word: ");
+	scanf("%s",word);
+	j=strlen(word)-1;
+	for(i=0;i<strlen(word);i++)
+	{
+		reverse[i]=word[j];
+		j--;
+	}
+	printf("Reverse Word: %s\n",reverse);
+	return 0;
+}
